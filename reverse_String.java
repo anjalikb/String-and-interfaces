d274bfcaff5488b23d01c2f83f09e5aa437ad9d1
Anjali Gunjal <107025907+anjalikb@users.noreply.github.com>
2022-08-03 12:20:26 +0530

    Add files via upload

---
+++ reverse_String.java
@@ -0,0 +1,15 @@
+package String_assignment;
+
+public class reverse_String {
+
+	public static void main(String[] args) 
+	{
+		int i=0;
+		String s1="MY NAME IS ABC";
+		for(i=s1.length()-1;i>0;i--)
+		{
+				System.out.println(s1.charAt(i));
+		}
+	}
+
+}
